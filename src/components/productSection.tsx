9deff505bb169a1f1be662a0809587d4a08a27b1
Ewulu Kenechukwu <132517562+KendoCee25@users.noreply.github.com>
2025-05-25 05:31:06 -0700

    Update productSection.tsx

--- src/components/productSection.tsx
+++ src/components/productSection.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState, useEffect } from 'react';
 import { useGetProductsQuery } from '../features/productsApi';
 import classes from '../components/productSection.module.css'
